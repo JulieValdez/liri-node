c35299e84238a59bf64dd411bc65e8076faee85e
JulieValdez <juliegragg0605@gmail.com>
2020-05-29 17:10:43 -0700

    more functions done

--- liri.js
+++ liri.js
@@ -5,6 +5,8 @@ var moment = require("moment");
 var Spotify = require("node-spotify-api");
 var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
+var functionCall = process.argv[3];
 
 //bands in town - artist, song name, preview link of song from spotify, album, default to the sign, ace of base
 //omdb - title, year, IMDB rating, rotten tomatoes rating, country produced, language, plot, actors, default to Mr. Nobody
@@ -16,7 +18,7 @@ var omdbFunction = function (movieName) {
     "http://www.omdbapi.com/?t=" +
     movieName +
     "&y=&plot=short&tomatoes=true&apikey=trilogy";
-
+  //axios call and then return of data
   axios.get(movieQueryUrl).then(function (response) {
     var data = response.data;
     if (response.data === true) {
@@ -37,31 +39,57 @@ var omdbFunction = function (movieName) {
 
 //spotify search and function
 var spotifyFunction = function (songName) {
-    
   spotify.search({ type: "track", query: songTitle }, function (err, data) {
     if (err) {
       return console.log("Error occured: " + err);
     }
   });
+  var songs = data.tracks.items;
+  for (let i = 0; i < songs.length; i++) {
+    const songsData = songs[i];
+    console.log(i);
+    console.log("artist(s): " + songsData[i].artists);
+    console.log("song name: " + songsData[i].name);
+    console.log("preview song: " + songsData[i].preview_url);
+    console.log("album: " + songsData[i].album.name);
+  }
+};
+
+//bands in town function
+var bandsintownFunction = function (artist) {
+  var concertQueryUrl =
+    "https://rest.bandsintown.com/artists/" +
+    artist +
+    "/events?app_id=codingbootcamp";
+  //axios call and return of data
+  axios.get(concertQueryUrl).then(function (response) {
+    var data = response.data;
+    for (let i = 0; i < data.length; i++) {
+      const concert = data[i];
+      console.log(artist);
+      console.log(concert.venue.city);
+      console.log(show.venue.region);
+      console.log(moment(show.datetime).format("MM/D/YYYY"));
+    }
+  });
 };
 
 //switch statementfor each of the commands for each of the calls
-var command = function (caseData, functionData) {
-  switch (casedata) {
-    case "concert-this":
-      bandsintownFunction();
-      break;
-    case "spotify-this-song":
-      spotifyFunction();
-      break;
-    case "movie-this":
-      omdbFunction();
-      break;
-    case "do-what-it-says":
-      doWhatItSaysFunction();
-      break;
-    default:
-      console.log("LIRI doesn't know that");
-      break;
-  }
-};
+
+switch (command) {
+  case "concert-this":
+    bandsintownFunction();
+    break;
+  case "spotify-this-song":
+    spotifyFunction();
+    break;
+  case "movie-this":
+    omdbFunction();
+    break;
+  case "do-what-it-says":
+    doWhatItSaysFunction();
+    break;
+  default:
+    console.log("LIRI doesn't know that");
+    break;
+}
